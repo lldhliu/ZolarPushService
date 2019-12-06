785916fe019571764dd47d89bb21365f95421978
Devin Liu <ldh1151375085@163.com>
2019-12-06 19:08:48 +0800

    更改密码规则

--- app/forms/user.py
+++ app/forms/user.py
@@ -6,7 +6,7 @@ class LoginForm(Form):
     phone = StringField(validators=[Regexp(r'1[85347]\d{9}', message='手机号码格式不正确')])
 
     password = PasswordField('密码', validators=[
-        Regexp(r'^[a-z0-9][a-z0-9_]{5}$', message='密码里面只能包含大小写字母、数字、_，且不能以“_”开头'),
+        Regexp(r'^[a-zA-Z0-9][a-zA-Z0-9_]{5, 15}$', message='密码里面只能包含大小写字母、数字、_，且不能以“_”开头'),
         DataRequired(message='密码不可以为空, 请输入你的密码'),
-        Length(6, 6, message='密码长度必须为6个字符')
+        Length(6, 16, message='密码长度最少为6个字符，最多为16个字符')
     ])
